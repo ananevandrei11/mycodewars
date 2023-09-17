9fd9b2307074c601c15e0407c9fa4a5d83ec9827
ananevandrei11 <ujlpjcbjlf2011@gmail.com>
2023-09-17 15:24:02 +0200

    input animation react-hook-form part 2

--- app/src/process/Components/Slug/InputAnimationWithHTMLCSS/index.tsx
+++ app/src/process/Components/Slug/InputAnimationWithHTMLCSS/index.tsx
@@ -1,37 +1,95 @@
 import Head from "next/head";
-import { PageProps } from "../../types";
 import { Container } from "@chakra-ui/react";
-import s from "./style.module.css";
+import { DetailedHTMLProps, InputHTMLAttributes, useId } from "react";
 import {
-  DetailedHTMLProps,
-  FormEvent,
-  InputHTMLAttributes,
-  useId,
-} from "react";
+  DeepMap,
+  FieldError,
+  FieldValues,
+  Path,
+  RegisterOptions,
+  UseFormRegister,
+  useForm,
+} from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import s from "./style.module.css";
+import { PageProps } from "../../types";
 
-type InputCustomProps = DetailedHTMLProps<
+const schema = z.object({
+  email: z
+    .string()
+    .nonempty({ message: "The email is required" })
+    .email({ message: "The email has invalid format" }),
+  password: z
+    .string()
+    .nonempty({ message: "The password is required" })
+    .min(6, { message: "The password cannot be less than 6 characters" }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+type InputProps = DetailedHTMLProps<
   InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
-> & {
+>;
+
+type InputCustomProps<T extends FieldValues> = Omit<InputProps, "name"> & {
   label: string;
+  name: Path<T>;
+  register: UseFormRegister<T>;
+  errors?: Partial<DeepMap<T, FieldError>>;
+  rules?: RegisterOptions;
 };
 
-const Input = ({ id, label, ...props }: InputCustomProps) => (
-  <div className={s.field}>
-    <input {...props} id={id} className={s.input} />
-    <label htmlFor={id} className={s.label}>
-      {label}
-    </label>
+const Input = <T extends Record<string, unknown>>({
+  id,
+  label,
+  name,
+  register,
+  rules,
+  errors,
+  ...props
+}: InputCustomProps<T>) => (
+  <div className={s.rootInput}>
+    <div className={s.field}>
+      <input
+        id={id}
+        className={s.input}
+        aria-describedby={`${id}-error-message`}
+        {...props}
+        {...register(name, rules)}
+      />
+      <label htmlFor={id} className={s.label}>
+        {label}
+      </label>
+    </div>
+    {errors && errors[name] && (
+      <p role="alert" id={`${id}-error-message`} className={s.error}>
+        {errors[name]?.message}
+      </p>
+    )}
   </div>
 );
 
 const InputAnimationWithHTMLCSS = ({ page }: PageProps) => {
   const idForm = useId();
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    console.log(formData);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    mode: "onSubmit",
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log("Submit", data);
   };
+
   return (
     <>
       <Head>
@@ -40,24 +98,33 @@ const InputAnimationWithHTMLCSS = ({ page }: PageProps) => {
       <Container>
         <div className={s.root}>
           <div className={s.wrapper}>
-            <form onSubmit={handleSubmit} className={s.form}>
+            <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
               <h1 className={s.title}>Sign In</h1>
-              <Input
+
+              <Input<FormData>
                 id={`${idForm}-email`}
-                name={`${idForm}-email`}
                 label="Email"
                 type="email"
+                name="email"
                 placeholder=" "
-                required
+                register={register}
+                errors={errors}
+                aria-invalid={errors.email ? "true" : "false"}
+                autoComplete="email"
               />
-              <Input
+
+              <Input<FormData>
                 id={`${idForm}-password`}
-                name={`${idForm}-password`}
                 label="Password"
                 type="password"
                 placeholder=" "
-                required
+                name="password"
+                errors={errors}
+                register={register}
+                aria-invalid={errors.password ? "true" : "false"}
+                autoComplete="current-password"
               />
+
               <input type="submit" className={s.submit} value="Sign In" />
             </form>
           </div>
